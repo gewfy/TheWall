de7ecab71c760ef5e45896747f5099cd664d8b23
tbleckert <tbleckert@gmail.com>
2012-05-03 13:33:03 +0200

    Major sketchboard update
    * You can now change brush size, color and style
    * Moved sketchboard styles from lib/layout.css to its own css

--- public/js/actions/sketchboard/sketchboard.action.js
+++ public/js/actions/sketchboard/sketchboard.action.js
@@ -17,8 +17,11 @@
         $sketchboard = $('#tpl-action-sketchboard').tmpl().appendTo('body');
         
         $sketchboard
-          .on('click', '.action.publish',  self.publish)
-          .on('click', '.action.cancel',   self.closeSketchboard);
+          .on('click',  '.action.publish',        self.publish)
+          .on('click',  '.action.cancel',         self.closeSketchboard)
+          .on('click',  '.action.brush-settings', self.showBrushSettings)
+          .on('click',  '.popover > .close',      self.hidePopover)
+          .on('click',  '.action.colorpicker',    self.showColorpicker);
 
         self.initSketchboard();
       }
@@ -27,6 +30,112 @@
 
     this.initSketchboard = function() {
       $sketchboard.sketchboard({style: 'ribbon'})
+    };
+    
+    this.showBrushSettings = function () {
+      var $popover = $sketchboard.find('.toolbar.brushSettings'),
+          $button = $(this),
+          posLeft = $button.offset().left,
+          posTop  = $button.offset().top;
+      
+      if ($popover.length < 1) {
+        $sketchboard.find('.popover:visible').hide();
+        $popover = $('#tpl-action-sketchboard-popover').tmpl().css({top: posTop - 45, left: posLeft + 48}).appendTo($sketchboard);
+      } else {
+        $sketchboard.find('.popover:visible').hide();
+        $popover.show();
+      }
+      
+      $popover.on('change', '.slider > input', function () {
+        $sketchboard.attr('data-size', $(this).val()).trigger('sizechanged');
+      });
+      
+      $popover.on('change', '.brush-style', function () {
+        $sketchboard.attr('data-brush', $(this).val()).trigger('brushchanged');
+      });
+    };
+    
+    this.showColorpicker = function () {
+      var $colorpicker = $sketchboard.find('.toolbar.colorpicker'),
+          $button = $(this),
+          posLeft = $button.offset().left,
+          posTop  = $button.offset().top;
+      
+      if ($colorpicker.length < 1) {
+        $sketchboard.find('.popover:visible').hide();
+        $('#tpl-action-sketchboard-colorpicker').tmpl().css({top: posTop - 96, left: posLeft + 48}).appendTo($sketchboard);
+        self.colorpicker($button);
+      } else {
+        $sketchboard.find('.popover:visible').hide();
+        $colorpicker.show();
+      }
+    };
+    
+    this.componentToHex = function (c) {
+      var hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+    };
+    
+    this.rgbToHex = function (r, g, b) {
+      return "#" + self.componentToHex(r) + self.componentToHex(g) + self.componentToHex(b);
+    };
+    
+    this.colorpicker = function ($button) {
+      var colorpicker = $('#colorpicker').get(0),
+          context     = colorpicker.getContext('2d'),
+          image       = new Image(),
+          imageData,
+          color,
+          colorTimeout,
+          mousePressed = false;
+          
+      colorpicker.style.cursor = 'crosshair';
+          
+      image.onload = function() {
+        context.drawImage(image, 0, 0);
+      };
+      
+      image.src = '/css/actions/sketchboard/colorpicker.png';
+      
+      $(colorpicker).on('mousedown', function (e) {
+        clearTimeout(colorTimeout);
+        
+        var x = e.pageX - $(this).offset().left,
+            y = e.pageY - $(this).offset().top;
+            
+        imageData = context.getImageData(x, y, 1, 1).data;
+        color = self.rgbToHex(imageData[0], imageData[1], imageData[2]);
+        
+        $button.children('span').css('background-color', color);
+        
+        mousePressed = true;
+      });
+      
+      $(colorpicker).on('mousemove', function (e) {
+        if (!mousePressed) return;
+        
+        clearTimeout(colorTimeout);
+        
+        var x = e.pageX - $(this).offset().left,
+            y = e.pageY - $(this).offset().top;
+            
+        imageData = context.getImageData(x, y, 1, 1).data;
+        color = self.rgbToHex(imageData[0], imageData[1], imageData[2]);
+        
+        $button.children('span').css('background-color', color);
+      });
+      
+      $(colorpicker).on('mouseup', function (e) {
+        colorTimeout = setTimeout(function () {
+          $sketchboard.attr('data-color', color).trigger('colorchanged');
+        }, 300);
+        
+        mousePressed = false;
+      });
+    }
+    
+    this.hidePopover = function () {
+      $(this).closest('.popover').hide();
     };
     
     this.publish = function () {
