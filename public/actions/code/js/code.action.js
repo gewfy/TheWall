19da0e1daff0e96c0ded73df8f872015b78a1c5e
Jakob Öhman <jakob@vinnovera.se>
2012-08-20 16:14:56 +0200

    Code action is now a singleton but instantiatable (#11)

--- public/actions/code/js/code.action.js
+++ public/actions/code/js/code.action.js
@@ -1,107 +1,116 @@
 (function($, global) {
-  global.codeAction = function() {
-    var self = this,
+  var action = (function() {
+    return new function() {
+      // This is a singleton and the actual action
+      var self = this,
 
-        callback,
+          callback,
 
-        $editor,
-        editor,
+          $editor,
+          editor,
 
-        $context;
+          $context;
 
-    this.init = function(x, y, $editContext, publishCallback) {
+      this.init = function(x, y, $editContext, publishCallback) {
 
-      $context  = $editContext;
-      callback  = publishCallback;
+        $context  = $editContext;
+        callback  = publishCallback;
 
-      if (!$editor) {
-        $editor = $('#tpl-action-code').tmpl().appendTo('body');
+        if (!$editor) {
+          $editor = $('#tpl-action-code').tmpl().appendTo('body');
 
-        $editor
-          .on('click', '.action.publish',  self.publish)
-          .on('click', '.action.show',     self.showEditor)
-          .on('click', '.action.hide',     self.hideEditor)
-          .on('click', '.action.cancel',   self.closeEditor);
+          $editor
+            .on('click', '.action.publish',  self.publish)
+            .on('click', '.action.show',     self.showEditor)
+            .on('click', '.action.hide',     self.hideEditor)
+            .on('click', '.action.cancel',   self.closeEditor);
 
-        self.initEditor();
-        $editor.animate({ height: '338px' });
-      }
-    };
+          self.initEditor();
+        }
 
-    this.initEditor = function() {
-      var updateDelay;
+        self.showEditor();
+      };
 
-      editor = CodeMirror.fromTextArea(
-        $editor.find('.input > textarea').get(0),
-        {
-          mode:         'text/html',
-          lineNumbers:  true,
-          lineWrapping: true,
-          onChange:     function() {
-            clearTimeout(updateDelay);
-            updateDelay = setTimeout(function () {
-              self.updateContext(editor.getValue());
-            }, 300);
+      this.initEditor = function() {
+        var updateDelay;
+
+        editor = CodeMirror.fromTextArea(
+          $editor.find('.input > textarea').get(0),
+          {
+            mode:         'text/html',
+            lineNumbers:  true,
+            lineWrapping: true,
+            onChange:     function() {
+              clearTimeout(updateDelay);
+              updateDelay = setTimeout(function () {
+                self.updateContext(editor.getValue());
+              }, 300);
+            }
+          }
+        );
+      };
+
+      this.updateContext = function(html) {
+        var canvas    = $context.get(0),
+            document  = canvas.ownerDocument;
+
+        canvas.innerHTML = html;
+
+        var scripts = canvas.getElementsByTagName('script');
+        for ( var i = 0; scripts[i]; i++ ) {
+          var script = scripts[i];
+          if (!script.type || script.type.toLowerCase() === 'text/javascript') {
+            var data    = (script.text || script.textContent || script.innerHTML || ""),
+
+                head    = document.getElementsByTagName('head')[0] || document.documentElement,
+                element = document.createElement('script');
+
+            element.type = 'text/javascript';
+            element.appendChild(document.createTextNode(data));
+            head.insertBefore(element, head.firstChild);
+            head.removeChild(element);
           }
         }
-      );
-    };
+      };
 
-    this.updateContext = function(html) {
-      var canvas    = $context.get(0),
-          document  = canvas.ownerDocument;
+      this.publish = function() {
+        self.closeEditor();
+        callback();
+      };
 
-      canvas.innerHTML = html;
+      this.showEditor = function() {
+        $editor.find('.action.show')
+          .removeClass('show')
+          .addClass('hide')
+          .html('Hide');
 
-      var scripts = canvas.getElementsByTagName('script');
-      for ( var i = 0; scripts[i]; i++ ) {
-        var script = scripts[i];
-        if (!script.type || script.type.toLowerCase() === 'text/javascript') {
-          var data    = (script.text || script.textContent || script.innerHTML || ""),
+        $editor.animate({height: '343px'});
+      };
 
-              head    = document.getElementsByTagName('head')[0] || document.documentElement,
-              element = document.createElement('script');
+      this.hideEditor = function() {
+        $editor.find('.action.hide')
+          .removeClass('hide')
+          .addClass('show')
+          .html('Show');
 
-          element.type = 'text/javascript';
-          element.appendChild(document.createTextNode(data));
-          head.insertBefore(element, head.firstChild);
-          head.removeChild(element);
+        $editor.animate({height: '38px'});
+      };
+
+      this.closeEditor = function() {
+        $editor.remove();
+        $editor = null;
+      };
+
+      this.beforePublish = function() {
+        if ($editor) {
+          $editor.remove();
         }
       }
     };
+  })();
 
-    this.publish = function() {
-      self.closeEditor();
-      callback();
-    };
-
-    this.showEditor = function() {
-      $(this)
-        .removeClass('show')
-        .addClass('hide')
-        .html('Hide');
-
-      $editor.animate({height: '343px'});
-    };
-
-    this.hideEditor = function() {
-      $(this)
-        .removeClass('hide')
-        .addClass('show')
-        .html('Show');
-
-      $editor.animate({height: '38px'});
-    };
-
-    this.closeEditor = function() {
-      $editor.remove();
-      $editor = null;
-    };
-
-    this.beforePublish = function() {
-      if ($editor) {
-        $editor.remove();
-      }
-    }
+  global.codeAction = function() {
+    // This is the one that gets instantiated
+    return action;
   }
 })(jQuery, this);
